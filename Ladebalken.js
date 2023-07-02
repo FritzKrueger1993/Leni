bf1c1724989c97245366ce10234ef0891ba42c1c
FritzKrueger1993 <109686983+FritzKrueger1993@users.noreply.github.com>
2023-07-02 21:30:27 +0200

    Update Ladebalken.js

--- Ladebalken.js
+++ Ladebalken.js
@@ -27,8 +27,8 @@ function setup() {
   progBarX = barX * 0.99;
   barY = 55;
   progBarY = barY - (barX - progBarX);
-  finalDate = new Date(2023,6,1);
-  startDate = new Date(2023,5,6);
+  finalDate = new Date(2023,6,7);
+  startDate = new Date(2023,6,2);
   background(3, 1, 10, 255);
 }
 
